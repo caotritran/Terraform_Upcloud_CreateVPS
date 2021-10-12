9757cd3822be4e1f08f32cee93769b0956326201
caotritran <caotritran.14@gmail.com>
2021-10-12 16:08:04 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ sudo rm -rf .terraform/ terraform.tfstate terraform.tfstate.backup .terraform.lo
 
 /usr/local/bin/terraform apply --auto-approve
 
-sudo echo "deploy_user ansible_host=`terraform output -raw publicIP` ansible_ssh_pass=\"`terraform output -raw password`\" ansible_user=root ansible_port=22" > ./ansible_adduser/iplist.txt
+sudo echo "deploy_user ansible_host=`/usr/local/bin/terraform output -raw publicIP` ansible_ssh_pass=\"`/usr/local/bin/terraform output -raw password`\" ansible_user=root ansible_port=22" > ./ansible_adduser/iplist.txt
 sudo ansible-playbook -i ./ansible_adduser/iplist.txt ./ansible_adduser/main.yml
